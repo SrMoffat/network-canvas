44cb13b7c565fca707b9ff7e63db246d548d5973
SrMoffat <ngigemoffat@gmail.com>
2023-07-18 16:08:22 +0300

    chore(setup): cleanup theme provider

--- app/providers/theme.tsx
+++ app/providers/theme.tsx
@@ -1,32 +1,19 @@
 'use client';
-import { useState, createContext } from 'react';
+import { useState, useEffect, createContext } from 'react';
 
 export const ThemeContext = createContext({});
 
 // @ts-ignore
 export function ThemeProvider({ children }) {
   const [theme, setTheme] = useState('light');
-  return <ThemeContext.Provider value={{ theme, setTheme }}>{children}</ThemeContext.Provider>;
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+  useEffect(() => {
+    const html = document?.querySelector('html');
+    if (html) {
+      html.setAttribute('data-theme', theme);
+    }
+  }, [theme]);
+  return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
 }
-
-
-
-// function MyAwesomeThemeComponent() {
-//   const toggleTheme = () => {
-//     setTheme(theme === 'dark' ? 'light' : 'dark');
-//   };
-//   // initially set the theme and "listen" for changes to apply them to the HTML tag
-//   useEffect(() => {
-//     const html = document?.querySelector('html')
-//     if (html) {
-//       html.setAttribute('data-theme', theme);
-//     }
-//   }, [theme]);
-//   return (
-//     <label className="swap swap-rotate">
-//       <input onClick={toggleTheme} type="checkbox" />
-//       <div className="swap-on">DARKMODE</div>
-//       <div className="swap-off">LIGHTMODE</div>
-//     </label>
-//   );
-// }
