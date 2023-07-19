f0df57b0cd682e859ab28e66ef531d43189a4d1a
4fr0c0d3 <31407254+SrMoffat@users.noreply.github.com>
2023-07-20 00:35:53 +0300

    Auth Views (#17)
    chore(theme): remove redundant theme
    chore(landing): route to auth pages from landing
    feat(signup): add signup view UI
    feat(login): add login view UI
    feat(loading): add loading page
    feat(footer): add footer to pages
    fix(navbar): adjust font weight and padding

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Footer, Nav } from '@/app/components';
+import LoginContent from './Content';
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <Nav />
+      <LoginContent />
+      <Footer />
+    </div>
   );
 }
+
---
+++ app/components/footer/index.tsx
@@ -0,0 +1,15 @@
+import Logo from "../navbar/Logo";
+
+const Footer = () => (
+    <footer className="footer footer-center p-10">
+        <div>
+            <Logo />
+            <p className="font-bold">
+                Network Canvas. <br />Providing JSON archiving solution.
+            </p>
+            <p>Copyright © 2023 - All right reserved</p>
+        </div>
+    </footer>
+);
+
+export default Footer;
