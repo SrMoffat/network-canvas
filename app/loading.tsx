37274793ba508eddceecd64945d83c316c356927
SrMoffat <ngigemoffat@gmail.com>
2023-07-20 00:21:20 +0300

    feat(loading): add loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,11 @@
 export default function Loading() {
   console.log('Loading...');
   return (
-    <span className="loading loading-spinner text-success" />
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="flex justify-center items-center space-x-1 text-sm">
+        <span className="loading loading-spinner text-success" />
+        <div>Loading ...</div>
+      </div>
+    </div>
   );
 }
