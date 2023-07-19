a5ee933276827ba98c6e3a9ac7bdb1af40eb6ba3
SrMoffat <ngigemoffat@gmail.com>
2023-07-19 21:21:56 +0300

    chore(setup): setup recent upload modal

--- app/components/navbar/Button.tsx
+++ app/components/navbar/Button.tsx
@@ -1,9 +1,34 @@
 'use client';
 import { useContext } from 'react';
 
+import { AiOutlinePaperClip } from 'react-icons/ai'
+
 import ThemeToggler from '@/app/components/theme/ThemeToggler';
 
 import { ThemeContext } from '@/app/providers';
+
+const NavBarContent = () => (
+  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+    <dd className="mt-2 text-sm sm:col-span-2 lg:col-span-4 sm:mt-0">
+      <ul role="list" className="divide-y rounded-md border">
+        <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm">
+          <div className="flex w-0 flex-1 items-center">
+            <AiOutlinePaperClip className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
+            <div className="ml-4 flex min-w-0 flex-1 gap-2">
+              <span className="truncate font-medium">socialgraph_data.json</span>
+              <span className="flex-shrink-0">2.4mb</span>
+            </div>
+          </div>
+          <div className="ml-4 flex-shrink-0">
+            <a href="#" className="font-medium">
+              Download
+            </a>
+          </div>
+        </li>
+      </ul>
+    </dd>
+  </div>
+);
 
 const NavBarButton = () => {
   const modalId = 'recent-upload';
@@ -17,7 +42,7 @@ const NavBarButton = () => {
       <input type="checkbox" id={modalId} className="modal-toggle" />
       <div className="modal">
         <div className="modal-box">
-          <p>Enim dolorem dolorum omnis atque necessitatibus. Consequatur aut adipisci qui iusto illo eaque. Consequatur repudiandae et. Nulla ea quasi eligendi. Saepe velit autem minima.</p>
+          < NavBarContent />
           <div className="modal-action">
             <label htmlFor={modalId} className="btn btn-outline" onClick={handleDownload}>Donwload</label>
             <label htmlFor={modalId} className="btn btn-neutral">Cancel</label>
