0330d64e2e1a206b05913cf76924dfefa3a19e0e
4fr0c0d3 <31407254+SrMoffat@users.noreply.github.com>
2023-07-20 00:35:53 +0300

    Auth Views (#17)
    chore(theme): remove redundant theme
    chore(landing): route to auth pages from landing
    feat(signup): add signup view UI
    feat(login): add login view UI
    feat(loading): add loading page
    feat(footer): add footer to pages
    fix(navbar): adjust font weight and padding

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { Nav, Hero } from '@/app/components';
+import { Nav, Hero, Footer } from '@/app/components';
 
 export default function Home() {
   return (
     <div>
       <Nav />
       <Hero />
+      <Footer />
     </div>
   );
 }
