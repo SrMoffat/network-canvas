9b81aec8ba7b3c8a1a387c5ccb7a1404829512b8
SrMoffat <ngigemoffat@gmail.com>
2023-07-20 11:11:46 +0300

    chore(setup): add file upload table for homepage

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AiOutlineHome } from 'react-icons/ai';
+import { AiOutlineHome, AiOutlineDelete, AiOutlineCloudDownload } from 'react-icons/ai';
 import { AuthNav, AuthNavDrawer } from '../components';
 
 const Breadcrumbs = () => (
@@ -17,8 +17,96 @@ const Breadcrumbs = () => (
 );
 
 const HomePageContent = () => (
-  <div className="px-6 border">
+  <div className="px-6">
     <Breadcrumbs />
+    <div className="flex flex-col w-full py-4">
+      <div className="grid h-20 card bg-base-300 rounded-box place-items-center">content</div>
+      <div className="divider"></div>
+      <div>Uploaded Files</div>
+      <div className="grid h-100 card bg-base-300 rounded-box place-items-center">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Filename</th>
+              <th>Uploaded</th>
+              <th>Actions</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div>
+                    <div className="font-bold">Hart Hagerty</div>
+                    <div className="text-sm opacity-50">United States</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Zemlak, Daniel and Leannon
+                <br />
+                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+              </td>
+              <td>
+                <button className="btn mr-2">
+                  <AiOutlineCloudDownload size="20" />
+                </button>
+                <button className="btn mr-2">
+                  <AiOutlineDelete size="20" />
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div>
+                    <div className="font-bold">Hart Hagerty</div>
+                    <div className="text-sm opacity-50">United States</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Zemlak, Daniel and Leannon
+                <br />
+                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+              </td>
+              <td>
+                <button className="btn mr-2">
+                  <AiOutlineCloudDownload size="20" />
+                </button>
+                <button className="btn mr-2">
+                  <AiOutlineDelete size="20" />
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div>
+                    <div className="font-bold">Hart Hagerty</div>
+                    <div className="text-sm opacity-50">United States</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Zemlak, Daniel and Leannon
+                <br />
+                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+              </td>
+              <td>
+                <button className="btn mr-2">
+                  <AiOutlineCloudDownload size="20" />
+                </button>
+                <button className="btn mr-2">
+                  <AiOutlineDelete size="20" />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 );
 export default function Home() {
