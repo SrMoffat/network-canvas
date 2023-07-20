57e92b5199091ac50409615386c340d0f20a9af5
SrMoffat <ngigemoffat@gmail.com>
2023-07-20 09:56:44 +0300

    chore(file); upload file dialogue

--- app/components/authNavBar/Drawer.tsx
+++ app/components/authNavBar/Drawer.tsx
@@ -5,8 +5,9 @@ const SideDrawer = () => (
       <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
       <ul className="menu p-4 w-80 h-full bg-base-200">
         <HeaderLogo classNames="" />
-        <li><a>Sidebar Item 1</a></li>
-        <li><a>Sidebar Item 2</a></li>
+        <li><a>Home</a></li>
+        <li><a>Files</a></li>
+        <li><a>Users</a></li>
       </ul>
     </div>
   );
