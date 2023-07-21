981c863265c3cb34f55d329e9d05635a2d75a5bb
SrMoffat <ngigemoffat@gmail.com>
2023-07-21 15:47:01 +0300

    feat(auth): add auth middleware for role checking

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+// export { default } from "next-auth/middleware";
+
+// export const config = {
+//     matcher: ['/auth/admin', '/home', '/files', '/users']
+// };
+
+import { NextResponse } from "next/server";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth(
+    // `withAuth` augments your `Request` with the user's token.
+    function middleware(req) {
+        console.log("Middleware called with", req.nextauth)
+        if (req.nextUrl.pathname === '/api/admin' && req.nextauth.token?.role !== 'admin') {
+            return new NextResponse("You are not authorized to access this")
+        }
+    },
+    {
+        callbacks: {
+            authorized: (params) => {
+                console.log("Middleware authorized with", params)
+                return !!params.token
+            },
+            //   authorized: ({ token }) => token?.role === "admin",
+        },
+    }
+)
+
+export const config = { matcher: ["/api/admin", "/files", "/users"] }
